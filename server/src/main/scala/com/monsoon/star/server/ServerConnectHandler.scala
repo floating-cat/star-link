1f0c8f5abd76ab0c48375058ed49366eef773c35
Lejia Chen <monsoon.cl@gmail.com>
2020-05-16 22:03:07 +0800

    Follow the WebSocket protocol not to send the data before success handshake.
    Caddy won't forward the following data packets before it receives the WebSocket handshake response

--- server/src/main/scala/com/monsoon/star/server/ServerConnectHandler.scala
+++ server/src/main/scala/com/monsoon/star/server/ServerConnectHandler.scala
@@ -12,34 +12,35 @@ final class ServerConnectHandler extends ChannelInboundHandlerAdapter {
   private var receivedClientHello = false
   private var buffer: ByteBuf = Unpooled.EMPTY_BUFFER
 
-  override def channelRead(ctx: ChannelHandlerContext, msg: Any): Unit = {
+  override def channelRead(inContext: ChannelHandlerContext, msg: Any): Unit = {
     if (!receivedClientHello) {
       receivedClientHello = true
 
       val request = msg.asInstanceOf[Socks5CommandRequest]
-      val promise = ctx.executor.newPromise[Channel]
+      val promise = inContext.executor.newPromise[Channel]
       promise.addListener((future: Future[Channel]) => {
         val outChannel = future.getNow
         if (future.isSuccess) {
-          outChannel.pipeline().addLast(new RelayHandler(ctx.channel, RelayTag.ServerSender))
-          ctx.pipeline().addLast(new RelayHandler(outChannel, RelayTag.ServerReceiver))
-          ctx.pipeline().remove(this)
-          outChannel.writeAndFlush(buffer)
+          inContext.pipeline()
+            .addLast(new RelayHandler(outChannel, RelayTag.ServerReceiver))
+            .remove(this)
+          outChannel.pipeline()
+            .addLast(new RelayHandler(inContext.channel, RelayTag.ServerSender))
+            .writeAndFlush(buffer)
         } else {
-          ChannelUtil.closeOnFlush(ctx.channel)
+          ChannelUtil.closeOnFlush(inContext.channel)
           buffer.release()
         }
       })
 
-      val inboundChannel = ctx.channel
+      val inboundChannel = inContext.channel
       new Bootstrap().group(inboundChannel.eventLoop)
         .channel(Engine.Default.socketChannelClass)
         .option[Integer](ChannelOption.CONNECT_TIMEOUT_MILLIS, TimeoutUtil.ConnectTimeoutMillis)
         .handler(new DirectClientHandler(promise))
         .connect(request.dstAddr, request.dstPort).addListener((future: ChannelFuture) => {
         if (!future.isSuccess) {
-          ChannelUtil.closeOnFlush(ctx.channel)
-
+          ChannelUtil.closeOnFlush(inContext.channel)
           buffer.release()
         }
       })
