320164ea8ad4de8db07a35fa5974f3d2ec4503fa
floating-cat <monsoon.cl@gmail.com>
2020-05-17 17:07:40 +0800

    Rename Engine for clarify

--- common/src/main/scala/com/monsoon/star/NettyEngine.scala
+++ common/src/main/scala/com/monsoon/star/NettyEngine.scala
@@ -7,7 +7,7 @@ import io.netty.channel.socket.nio.{NioServerSocketChannel, NioSocketChannel}
 import io.netty.channel.socket.{ServerSocketChannel, SocketChannel}
 import io.netty.channel.{EventLoopGroup, epoll, kqueue}
 
-sealed trait Engine {
+sealed trait NettyEngine {
   def eventLoopGroup(nThreads: Int = 0): EventLoopGroup
 
   val serverSocketChannelClass: Class[_ <: ServerSocketChannel]
@@ -15,30 +15,30 @@ sealed trait Engine {
   val socketChannelClass: Class[_ <: SocketChannel]
 }
 
-object Engine {
+object NettyEngine {
 
-  val Default: Engine = if (epoll.Epoll.isAvailable)
+  val Default: NettyEngine = if (epoll.Epoll.isAvailable)
     Epoll
   else if (kqueue.KQueue.isAvailable)
     KQueue
   else
     Nio
 
-  private object Nio extends Engine {
+  private object Nio extends NettyEngine {
     override def eventLoopGroup(nThreads: Int = 0) = new NioEventLoopGroup(nThreads)
 
     override val serverSocketChannelClass = classOf[NioServerSocketChannel]
     override val socketChannelClass = classOf[NioSocketChannel]
   }
 
-  private object Epoll extends Engine {
+  private object Epoll extends NettyEngine {
     override def eventLoopGroup(nThreads: Int = 0) = new EpollEventLoopGroup(nThreads)
 
     override val serverSocketChannelClass = classOf[EpollServerSocketChannel]
     override val socketChannelClass = classOf[EpollSocketChannel]
   }
 
-  private object KQueue extends Engine {
+  private object KQueue extends NettyEngine {
     override def eventLoopGroup(nThreads: Int = 0): EventLoopGroup = new KQueueEventLoopGroup(nThreads)
 
     override val serverSocketChannelClass = classOf[KQueueServerSocketChannel]
