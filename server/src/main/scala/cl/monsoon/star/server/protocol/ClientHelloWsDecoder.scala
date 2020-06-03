56137ee94ba94c0b5186f2fb86bc90480b17200a
floating-cat <monsoon.cl@gmail.com>
2020-06-03 22:08:26 +0800

    Fix the incorrect code style

--- server/src/main/scala/cl/monsoon/star/server/protocol/ClientHelloWsDecoder.scala
+++ server/src/main/scala/cl/monsoon/star/server/protocol/ClientHelloWsDecoder.scala
@@ -6,7 +6,7 @@ import java.util.Base64
 
 import cl.monsoon.star._
 import cl.monsoon.star.config.Password
-import cl.monsoon.star.server.protocol.ClientHelloWsDecoder.{ClientHelloInfoRegex, PasswordRegex, WsResponse}
+import cl.monsoon.star.server.protocol.ClientHelloWsDecoder.{clientHelloInfoRegex, passwordRegex, wsResponse}
 import io.netty.buffer.{ByteBuf, Unpooled}
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageDecoder
@@ -26,19 +26,19 @@ final class ClientHelloWsDecoder(password: Password) extends ByteToMessageDecode
     result match {
       case Value(v) if first =>
         first = false
-        val pass = PasswordRegex.findFirstMatchIn(v)
+        val pass = passwordRegex.findFirstMatchIn(v)
           .filter(m => Base64.getDecoder.decode(m.group(1)).sameElements(password.value))
         if (pass.isEmpty) ctx.close()
 
-      case Value(ClientHelloInfoRegex(v)) =>
+      case Value(clientHelloInfoRegex(v)) =>
         val buf = Unpooled.wrappedBuffer(Base64.getDecoder.decode(v))
         clientHelloInfo = Some(ClientHelloInfoDecoder.decodeCommand(buf))
 
       case Value(_) | Suspension =>
 
       case End if !first && clientHelloInfo.nonEmpty =>
-        WsResponse.retain()
-        ctx.writeAndFlush(WsResponse.duplicate())
+        wsResponse.retain()
+        ctx.writeAndFlush(wsResponse.duplicate())
         out.add(clientHelloInfo.get)
 
         ctx.pipeline().remove(this)
@@ -55,11 +55,11 @@ final class ClientHelloWsDecoder(password: Password) extends ByteToMessageDecode
 }
 
 object ClientHelloWsDecoder {
-  private val PasswordRegex: Regex = "GET /([^ ]*) ".r
-  private val ClientHelloInfoRegex: Regex = "X-A:[ ]*([^ ]*)[ ]*".r
+  private val passwordRegex: Regex = "GET /([^ ]*) ".r
+  private val clientHelloInfoRegex: Regex = "X-A:[ ]*([^ ]*)[ ]*".r
 
   // may increase the response length to avoid the side-channel leaks
-  private val WsResponse: ByteBuf =
+  private val wsResponse: ByteBuf =
     Unpooled.unreleasableBuffer(Unpooled.wrappedBuffer(("HTTP/1.1 101 Switching Protocols\r\n" +
       "Upgrade:websocket\r\n" +
       "Connection:Upgrade\r\n\r\n")
