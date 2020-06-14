734c4ff356ec48bf1a7b677ee12ee7a28899d9f0
floating-cat <monsoon.cl@gmail.com>
2020-06-14 19:18:37 +0800

    Support system proxy for Gnome & KDE

--- common/src/main/scala/cl/monsoon/star/BootstrapUtil.scala
+++ common/src/main/scala/cl/monsoon/star/BootstrapUtil.scala
@@ -3,18 +3,23 @@ package cl.monsoon.star
 import java.net.SocketAddress
 
 import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.ChannelHandler
+import io.netty.channel.{ChannelFuture, ChannelHandler}
 
 object BootstrapUtil {
 
-  def server(socketAddress: SocketAddress, childHandler: ChannelHandler): Unit = {
+  def server(socketAddress: SocketAddress, childHandler: ChannelHandler, bindAction: => Unit = ()): Unit = {
     val group = NettyEngine.Default.eventLoopGroup(1)
     try {
       new ServerBootstrap()
         .group(group)
         .channel(NettyEngine.Default.serverSocketChannelClass)
         .childHandler(childHandler)
-        .bind(socketAddress).sync()
+        .bind(socketAddress)
+        .addListener { future: ChannelFuture =>
+          if (future.isSuccess) {
+            bindAction
+          }
+        }.sync()
         .channel()
         .closeFuture().sync()
     } finally {
