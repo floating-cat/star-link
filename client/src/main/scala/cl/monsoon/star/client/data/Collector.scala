e4145fcac6dd862b34bd00ea262033aedfe71da7
floating-cat <monsoon.cl@gmail.com>
2020-06-27 15:15:30 +0800

    Add release script

---
+++ client/src/main/scala/cl/monsoon/star/client/data/Collector.scala
@@ -0,0 +1,28 @@
+package cl.monsoon.star.client.data
+
+import java.net.URL
+import java.nio.file.{Files, Paths}
+
+import scala.io.Source
+import scala.jdk.CollectionConverters._
+import scala.util.Using
+
+trait Collector[A] {
+
+  val resPath: String
+  val validator: String => A
+
+  // Paths#get doesn't work for jar.
+  lazy val outputUrl: URL = getClass.getResource(resPath)
+
+  final def write(outputDir: Option[String], output: Iterator[String]): Unit = {
+    val path = outputDir.fold(Paths.get(outputUrl.toURI))(Paths.get(_, resPath))
+    Files.write(path, output.to(Iterable).asJava)
+  }
+
+  final def get(): List[A] = {
+    Using.resource(Source.fromURL(outputUrl)) { source =>
+      source.getLines().map(validator).toList
+    }
+  }
+}
---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+script_dir=$(dirname "${BASH_SOURCE[0]}")
+export JAVA_OPTS="$JAVA_OPTS -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=1080"
+sudo systemctl start docker.service
+sbt "client/runMain cl.monsoon.star.client.data.CnDomainSuffixCollector $script_dir/../client/src/main/resources; \
+client/runMain cl.monsoon.star.client.data.CnIpCidrCollector $script_dir/../client/src/main/resources; \
+universal:packageBin; \
+docker:publish"
+sudo systemctl stop docker.service
