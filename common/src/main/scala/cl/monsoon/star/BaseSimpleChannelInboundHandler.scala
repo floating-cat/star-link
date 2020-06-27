277b88617f5ef52eb1a4a02ee6fa0a009880d8ca
floating-cat <monsoon.cl@gmail.com>
2020-06-27 15:26:58 +0800

    Log local and remote addresses

--- common/src/main/scala/cl/monsoon/star/BaseSimpleChannelInboundHandler.scala
+++ common/src/main/scala/cl/monsoon/star/BaseSimpleChannelInboundHandler.scala
@@ -3,12 +3,15 @@ package cl.monsoon.star
 import grizzled.slf4j.Logger
 import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 
+import scala.util.chaining._
+
 abstract class BaseSimpleChannelInboundHandler[I] extends SimpleChannelInboundHandler[I] {
 
   private val logger = Logger[this.type]()
 
   override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
+    val (local, remote) = ctx.channel().pipe(c => (c.localAddress(), c.remoteAddress()))
     ctx.close()
-    logger.error("Uncaught exception", cause)
+    logger.error(s"Uncaught exception (Local and remote addresses: $local, $remote", cause)
   }
 }
