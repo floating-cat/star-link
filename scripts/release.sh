2a80c6f80a7385da25484985f26fcc3de54aa959
floating-cat <monsoon.cl@gmail.com>
2020-06-27 20:37:51 +0800

    Improve the packaging
    * Reduce docker image size.
    * Tweak the memory usage.

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,6 @@ export JAVA_OPTS="$JAVA_OPTS -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=1080"
 sudo systemctl start docker.service
 sbt "client/runMain cl.monsoon.star.client.data.CnDomainSuffixCollector $script_dir/../client/src/main/resources; \
 client/runMain cl.monsoon.star.client.data.CnIpCidrCollector $script_dir/../client/src/main/resources; \
-universal:packageBin; \
-docker:publish"
+link/universal:packageBin; \
+link/docker:publish"
 sudo systemctl stop docker.service
